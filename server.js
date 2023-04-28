5835899ac166081051b1eec7a3e4386f08171584
Yuvaraj V <112630545+Yuvaraj643@users.noreply.github.com>
2023-04-28 22:56:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,3 +31,9 @@ app.listen(PORT,() =>[
     console.log(`listening to the ${PORT}`)
 ])
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
